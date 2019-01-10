0990e0185bf4b25624b03f2d269157873dabfeca
JoJOes <margizia@localhost>
2019-01-10 15:59:04 +0100

    Form

--- src/theotherhattrick/MainWindow.java
+++ src/theotherhattrick/MainWindow.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MainWindow {
+public class MainWindow extends JFrame {
     private JPanel panel1;
     private JPanel bottomPanel;
     private JPanel leftPanel;
